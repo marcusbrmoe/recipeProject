37eff56faedb3bc7af5bdca452005b73dcbc181a
Marcus Moe <marcusmo1@hotmail.com>
2020-12-06 11:23:10 +0200

    Fixing tests

--- src/test/java/com/example/recipeProject/RecipeProjectApplicationTests.java
+++ src/test/java/com/example/recipeProject/RecipeProjectApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.recipeProject;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.recipeProject.web.RecipeController;
 
